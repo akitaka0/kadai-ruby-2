190c249ac6a85b2a15c44497dee2a998dedca05f
akitaka <syugen3619@gmail.com>
2021-06-27 13:38:58 +0000

    fizzbuzz

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,16 @@
+num_max = 100
+
+(1..num_max).each do |num|
+ def fizzbuzz(num)
+   if num % 15 == 0
+    "FizzBuzz"
+   elsif num % 3 == 0
+    "Fizz"
+   elsif num % 5 == 0
+    "Buzz"
+   else
+    num
+   end
+ end
+ puts fizzbuzz(num)
+end
